429836edb730924e196438ad634101f1661a2f67
Aditya Rajput <adiraj20072002@gmail.com>
2024-07-20 22:13:42 +0530

    feat(maths): Add addition and subtraction puzzles

--- lib/constants/predefined_puzzles.dart
+++ lib/constants/predefined_puzzles.dart
@@ -214,14 +214,38 @@ const directionsPuzzles = <DirectionsPuzzle>[
 ];
 
 const mathsPuzzles = <MathsPuzzle>[
-  // region faces
+  // region add
   MathsPuzzle(equation: '0 / 0 = ?', solution: ['0']),
   MathsPuzzle(equation: '0 / 1 = ?', solution: ['1']),
   MathsPuzzle(equation: '1 / 1 = ?', solution: ['2']),
   MathsPuzzle(equation: '2 / 1 = ?', solution: ['3']),
   MathsPuzzle(equation: '2 / 2 = ?', solution: ['4']),
   MathsPuzzle(equation: '1 / 0 = ?', solution: ['1']),
-  MathsPuzzle(equation: '3 / 1 = ?', solution: ['4']),
+  // endregion
+  // region carry
+  MathsPuzzle(equation: '? / 4 = 10', solution: ['1']),
+  MathsPuzzle(equation: '3 / ? = 10', solution: ['2']),
+  MathsPuzzle(equation: '44 / 23 = 1?2', solution: ['2']),
+  MathsPuzzle(equation: '1? / ?4 = ?01', solution: ['2', '3', '1']),
+  MathsPuzzle(equation: '?4 / 1? = 42', solution: ['2', '3']),
+  MathsPuzzle(equation: '22 / 23 = 1??', solution: ['0', '0']),
+  // endregion
+  // region subtract
+  MathsPuzzle(equation: r'2 \ 1 = ?', solution: ['1']),
+  MathsPuzzle(equation: r'3 \ 1 = ?', solution: ['2']),
+  MathsPuzzle(equation: r'10 \ 2 = ?', solution: ['3']),
+  MathsPuzzle(equation: r'4 \ 0 = ?', solution: ['4']),
+  MathsPuzzle(equation: r'101 \ 100 = ?', solution: ['1']),
+  MathsPuzzle(equation: r'34 \ 12 = ??', solution: ['2', '2']),
+  // endregion
+  // region borrow
+  MathsPuzzle(equation: r'13 \ 4 = ?', solution: ['4']),
+  MathsPuzzle(equation: r'31 \ ? = 24', solution: ['2']),
+  MathsPuzzle(equation: r'421 \ 124 = ???', solution: ['2', '4', '2']),
+  MathsPuzzle(equation: r'1? \ 2 = ?4', solution: ['1', '0']),
+  MathsPuzzle(equation: r'1000 \ 101 = ???', solution: ['3', '4', '4']),
+  MathsPuzzle(equation: r'?0? \ ?0 = 223', solution: ['3', '3', '3']),
   // endregion
   // TODO: Add more puzzles
+  // TODO: Don't reset MathsPuzzle score if wrong.
 ];
